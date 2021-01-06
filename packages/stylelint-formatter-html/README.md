b37c3e133b3938d6b01adb8834e3d291f7980f8e
fangt11 <fangt11@ke.com>
2021-01-06 10:41:04 +0800

    docs: 更新文档

--- packages/stylelint-formatter-html/README.md
+++ packages/stylelint-formatter-html/README.md
@@ -2,6 +2,8 @@
 
 -   一款更好的 stylelint 报告格式化工具。
 -   官方介绍请参考: [https://stylelint.io/developer-guide/formatters](https://stylelint.io/developer-guide/formatters)
+-   [demo](https://shuoshubao.github.io/nbfe/packages/stylelint-formatter-html/index.html)
+-   [github](https://github.com/shuoshubao/nbfe/tree/master/packages/stylelint-formatter-html)
 
 # 安装
 
--- packages/stylelint-formatter-html/docs/StylelintResults.js
+++ packages/stylelint-formatter-html/docs/StylelintResults.js
@@ -15,78 +15,7 @@ window.StylelintResults = [
         invalidOptionWarnings: [],
         parseErrors: [],
         errored: true,
-        css: `/*
- * @Description:
- * @version:
- * @Author: 吴婷婷
- * @Date: 2020-09-23 21:56:18
- * @LastEditTime: 2020-09-23 23:56:27
- */
-.modal-detail {
-    padding: 20px 20px 10px;
-
-    color: #222;
-    background-color: #f8f8f8;
-    .money {
-        margin-right: 5px;
-
-        font-weight: bold;
-        font-size: 18px;
-        font-family: PingFang SC;
-        line-height: 18px;
-
-
-        vertical-align: text-top
-    }
-    .label {
-        color: #999;
-    }
-}
-.modal-detail {
-    padding: 20px 20px 10px;
-
-    color: #222;
-    background-color: #f8f8f8;
-    .money {
-        margin-right: 5px;
-
-        font-weight: bold;
-        font-size: 18px;
-        font-family: PingFang SC;
-        line-height: 18px;
-
-
-        vertical-align: text-top
-    }
-    .label {
-        color: #999;
-    }
-    .modal-detail {
-        padding: 20px 20px 10px;
-
-        color: #222;
-        background-color: #f8f8f8;
-        .money {
-            margin-right: 5px;
-
-            font-weight: bold;
-            font-size: 18px;
-            font-family: PingFang SC;
-            line-height: 18px;
-
-
-            vertical-align: text-top
-        }
-        .label {
-            color: #999;
-        }
-    }
-}
-.modal-label {
-    display: inline-block;
-    width: 100px;
-}
-`,
+        css: '',
         warnings: [
             {
                 line: 3,
