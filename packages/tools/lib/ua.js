286c365ceddb774e5ef79db94311af1fe00f163a
fangt11 <fangt11@ke.com>
2021-07-26 14:10:38 +0800

    fix: isIPhoneX 变成方法调用形式

--- packages/tools/lib/ua.js
+++ packages/tools/lib/ua.js
@@ -17,10 +17,10 @@ export const isIPhone = () => {
     return /\biPhone\b|\biPod\b/i.test(ua);
 };
 
-export const isIPhoneX = (() => {
+export const isIPhoneX = () => {
     const { width, height } = window.screen;
     if (!isIPhone) {
         return false;
     }
     return (height === 812 && width === 375) || (height === 896 && width === 414);
-})();
+};
