bd5d5d92d795a9ffe0a7d4b615dba71defe05078
fangt11 <fangt11@ke.com>
2021-08-10 13:38:25 +0800

    docs: 发布文档

--- docs/assets/js/code.js
+++ docs/assets/js/code.js
@@ -2,7 +2,7 @@
  * @Author: fangt11
  * @Date:   2021-08-06 20:16:27
  * @Last Modified by:   fangt11
- * @Last Modified time: 2021-08-06 20:50:33
+ * @Last Modified time: 2021-08-10 13:18:05
  */
 
 const { React, antd } = window;
@@ -14,20 +14,34 @@ window.addEventListener('load', () => {
         const icon = e.target.closest('.anticon');
         // 展示源码
         if (icon && icon.classList.contains('action-showCode')) {
+            const { funcname, example } = icon.dataset;
+            const source = [
+                `require('lodash');`,
+                `const { ${funcname} } = require('@nbfe/tools');`,
+                example.replaceAll('__@@__', '\n'),
+                ''
+            ].join('\n');
+            const time = Date.now();
+            const domId = ['runkit', 'container', 'dom', time].join('-');
             Modal.info({
-                title: '源码',
-                width: 1000,
+                title: 'Try in REPL',
+                width: 1200,
                 okText: '知道了',
                 content: createElement(
-                    'pre',
+                    'div',
                     {
-                        style: {
-                            margin: 0
-                        }
+                        id: domId
                     },
-                    [icon.dataset.code]
+                    []
                 )
             });
+            setTimeout(() => {
+                RunKit.createNotebook({
+                    theme: 'atom-light-syntax',
+                    element: document.getElementById(domId),
+                    source
+                });
+            }, 0);
         }
     });
 });
