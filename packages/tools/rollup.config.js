e69d5af681dc4dd975a002e19b2dd1bfc05a998d
shuoshubao <759979885@qq.com>
2022-12-25 15:25:21 +0800

    fix(rollup): @rollup/plugin-terser

--- packages/tools/rollup.config.js
+++ packages/tools/rollup.config.js
@@ -2,7 +2,7 @@ import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import pkg from './package.json';
 
 const createComment = data => {
