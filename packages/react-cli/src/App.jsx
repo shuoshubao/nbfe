6b79a55488405551e97ff38cdea7e9b225d468bd
shuoshubao <759979885@qq.com>
2022-12-25 16:09:41 +0800

    docs: update demo

--- packages/react-cli/src/App.jsx
+++ packages/react-cli/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import CSSModules from 'react-css-modules';
 import ReactLogo from '@/assets/images/react.png';
 import DumiLogo from '@/assets/images/dumi.png';
-import styles from '@/assets/styles/index.module.less';
+import '@/assets/styles/1.css';
+import '@/assets/styles/1.less';
+import '@/assets/styles/1.scss';
+import '@/assets/styles/1.styl';
 
-@CSSModules(styles, { allowMultiple: true, handleNotFoundStyleName: 'ignore' })
 export default class extends React.Component {
     static displayName = 'App';
 
@@ -26,11 +27,14 @@ export default class extends React.Component {
         this.test();
         return (
             <div>
-                <div styleName="aa">App</div>
+                <div>App</div>
                 <img src={ReactLogo} />
                 <img src={DumiLogo} />
                 <div className="logo-png" style={{ height: 180 }}>
                     logo-png
+                </div>
+                <div className="logo-svg" style={{ height: 180 }}>
+                    logo-svg
                 </div>
             </div>
         );
--- packages/react-cli/src/index.js
+++ packages/react-cli/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import '@/assets/styles/1.css';
-import '@/assets/styles/1.less';
-import '@/assets/styles/2.styl';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
