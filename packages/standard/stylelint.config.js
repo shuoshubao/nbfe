5e061179ca811e5e767326864b23b79084894442
shuoshubao <shuoshubao@gmail.com>
2024-01-22 19:49:15 +0800

    feat: stylelint 调整规则

--- packages/standard/stylelint.config.js
+++ packages/standard/stylelint.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier', 'stylelint-config-rational-order'],
   plugins: ['stylelint-prettier'],
-  overrides: [
-    {
-      files: ['*.scss', '**/*.scss'],
-      customSyntax: 'postcss-scss'
-    },
-    {
-      files: ['*.less', '**/*.less'],
-      customSyntax: 'postcss-less'
-    }
-  ],
+  // overrides: [
+  //   {
+  //     files: ['**/*.scss'],
+  //     customSyntax: 'postcss-scss'
+  //   },
+  //   {
+  //     files: ['**/*.less'],
+  //     customSyntax: 'postcss-less'
+  //   }
+  // ],
   rules: {
     'max-nesting-depth': 10,
     'selector-pseudo-class-no-unknown': [
       true,
-      { ignorePseudoClasses: ['export', 'import', 'global', 'local', 'external'] }
+      { ignorePseudoClasses: ['export', 'import', 'global', 'local', 'external', 'deep'] }
     ],
     'comment-empty-line-before': null,
     'no-invalid-double-slash-comments': null,
@@ -24,6 +24,9 @@ module.exports = {
     'unit-no-unknown': null,
     'prettier/prettier': true,
     'font-family-no-missing-generic-family-keyword': null,
-    'selector-class-pattern': '^[a-zA-Z][-a-zA-Z0-9]+$'
+    'selector-class-pattern': '^[a-zA-Z][-_a-zA-Z0-9]+$',
+    'color-function-notation': 'legacy',
+    'import-notation': 'string',
+    'alpha-value-notation': 'number'
   }
 }
